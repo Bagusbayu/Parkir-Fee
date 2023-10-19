3a1c9d5ad5f75bd4f63365231a058d60cbf4aca9
Bagus Bayu Kencana <v.bagus.kencana@adira.co.id>
2023-10-19 11:37:19 +0700

    logic perhitungan parkir

--- parkir-fee.js
+++ parkir-fee.js
@@ -6,7 +6,17 @@ function hitungBiayaParkir(masuk, keluar) {
   const masukWaktu = moment(masuk, formatWaktu);
   const keluarWaktu = moment(keluar, formatWaktu);
   const durasiJam = keluarWaktu.diff(masukWaktu, 'hours', true);
+  // console.log('durasiJam: ', durasiJam);
+  let biaya = 7000; // Biaya awal
 
+  if (durasiJam > 12) {
+    biaya += 3000; // First hour after 12 hours
+    // biaya += 3000 * Math.floor(durasiJam - 12);
+    biaya += 3000 * Math.floor((durasiJam - 12) / 12);
+    // console.log('3000 * Math.floor((durasiJam - 12) / 24): ', 3000 * Math.floor((durasiJam - 12) / 24));
+  }
+
+ return biaya;
 }
 
 
@@ -14,4 +24,9 @@ const masuk = readline.question('Masukkan waktu parkir masuk (YYYY-MM-DD HH:mm:s
 const keluar = readline.question('Masukkan waktu parkir keluar (YYYY-MM-DD HH:mm:ss): ');
 
 const biaya = hitungBiayaParkir(masuk, keluar);
-console.log(`Biaya parkir: Rp ${biaya}`);
+
+if (biaya !== undefined) {
+  console.log(`Biaya parkir: Rp ${biaya}`);
+} else {
+  console.log('Input times are not valid. Please check the format (YYYY-MM-DD HH:mm:ss).');
+}
