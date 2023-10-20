179817a0457edbee3fed5e045379f36ad8fe12be
Bagus Bayu Kencana <v.bagus.kencana@adira.co.id>
2023-10-20 23:18:41 +0700

    menambahkan ganti kondisi - 45 menit

--- parkir-fee.js
+++ parkir-fee.js
@@ -6,19 +6,21 @@ function hitungBiayaParkir(masuk, keluar) {
   const masukWaktu = moment(masuk, formatWaktu);
   const keluarWaktu = moment(keluar, formatWaktu);
   const durasiJam = keluarWaktu.diff(masukWaktu, 'hours', true);
-  // console.log('durasiJam: ', durasiJam);
+  console.log('durasiJam: ', durasiJam);
   let biaya = 7000; // Biaya awal
 
   if (durasiJam > 12) {
-    biaya += 3000; // First hour after 12 hours
-    // biaya += 3000 * Math.floor(durasiJam - 12);
-    biaya += 3000 * Math.floor((durasiJam - 12) / 12);
-    // console.log('3000 * Math.floor((durasiJam - 12) / 24): ', 3000 * Math.floor((durasiJam - 12) / 24));
+    biaya += 3000;
+    if (durasiJam > 24) {
+      biaya += 7000;
+      if (durasiJam > 36) {
+        biaya += 3000 * Math.ceil((durasiJam - 36) / 12);
+      }
+    }
   }
 
  return biaya;
 }
-
 
 const masuk = readline.question('Masukkan waktu parkir masuk (YYYY-MM-DD HH:mm:ss): ');
 const keluar = readline.question('Masukkan waktu parkir keluar (YYYY-MM-DD HH:mm:ss): ');
