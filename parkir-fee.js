5a9e2ead16f4346f0cc442c5ee3370c1a1fb67b0
Bagus Bayu Kencana <v.bagus.kencana@adira.co.id>
2023-10-27 00:02:59 +0700

    final comit - 4 jam(2 jam & 2 jam)

--- parkir-fee.js
+++ parkir-fee.js
@@ -1,34 +1,42 @@
 const moment = require('moment');
 const readline = require('readline-sync');
 
-function hitungBiayaParkir(masuk, keluar) {
-  const formatWaktu = 'YYYY-MM-DD HH:mm:ss';
-  const masukWaktu = moment(masuk, formatWaktu);
-  const keluarWaktu = moment(keluar, formatWaktu);
-  const periodePertama = 12;  // Durasi periode pertama dalam jam
-  const hargaPertama = 7000;
-  const hargaTambahan = 3000;
-  let durasiJam = keluarWaktu.diff(masukWaktu, 'hours', true);
-  const hargaPerPeriode = [hargaPertama, hargaPertama + hargaTambahan];  // Harga per periode
-  let biaya = 0;
+function calculateParkingFee(date_start, date_end, first_price, next_price, first_period, second_period) {
+  const masukWaktu = moment(date_start, 'YYYY-MM-DD HH:mm:ss');
+  const keluarWaktu = moment(date_end, 'YYYY-MM-DD HH:mm:ss');
+  let total_hours = keluarWaktu.diff(masukWaktu, 'hours', true);
+  let total_amount = 0;
+  let amount_day = 0;
+  let amount_hour = 0;
 
-  while (durasiJam > 0) {
-    if (durasiJam <= periodePertama) {
-      biaya += hargaPerPeriode[0];
-    } else {
-      biaya += hargaPerPeriode[0];
-      biaya += hargaTambahan;
-    }
-    durasiJam -= periodePertama;
+  const days = Math.floor(total_hours / (first_period + second_period))
+  // console.log('days: ', days);
+
+  amount_day += days * (first_price + next_price)
+
+  const sisa_hour = total_hours % (first_period + second_period)
+  // console.log('sisa_hour: ', sisa_hour);
+
+  amount_hour += first_price
+
+  if(sisa_hour >= second_period){
+      amount_hour += next_price
   }
 
-  return biaya;
+  total_amount = amount_day + amount_hour
+
+  return total_amount;
 }
 
 const masuk = readline.question('Masukkan waktu parkir masuk (YYYY-MM-DD HH:mm:ss): ');
 const keluar = readline.question('Masukkan waktu parkir keluar (YYYY-MM-DD HH:mm:ss): ');
 
-const biaya = hitungBiayaParkir(masuk, keluar);
+const first_price = 7000;
+const next_price = 3000;
+const first_period = 12;
+const second_period = 12;
+
+const biaya = calculateParkingFee(masuk, keluar, first_price, next_price, first_period, second_period);
 
 if (biaya !== undefined) {
   console.log(`Biaya parkir: Rp ${biaya}`);
